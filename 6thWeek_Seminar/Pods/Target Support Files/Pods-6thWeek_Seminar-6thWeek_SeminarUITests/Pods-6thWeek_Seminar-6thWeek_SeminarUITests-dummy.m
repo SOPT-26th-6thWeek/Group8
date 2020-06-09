4a1609b2d5e8a327fc6ef2e806fb0b49a05d9138
seu11ee <yslee2504@naver.com>
2020-06-10 01:11:08 +0900

    pod 업데이트

---
+++ 6thWeek_Seminar/Pods/Target Support Files/Pods-6thWeek_Seminar-6thWeek_SeminarUITests/Pods-6thWeek_Seminar-6thWeek_SeminarUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_6thWeek_Seminar_6thWeek_SeminarUITests : NSObject
+@end
+@implementation PodsDummy_Pods_6thWeek_Seminar_6thWeek_SeminarUITests
+@end
