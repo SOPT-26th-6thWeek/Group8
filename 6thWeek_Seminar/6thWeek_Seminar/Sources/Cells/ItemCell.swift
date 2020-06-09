51a766f25e2f7a5ac3958b25a25fcbad750b92a1
seu11ee <yslee2504@naver.com>
2020-06-10 02:33:57 +0900

    윤진황호업뎃

--- 6thWeek_Seminar/6thWeek_Seminar/Sources/Cells/ItemCell.swift
+++ 6thWeek_Seminar/6thWeek_Seminar/Sources/Cells/ItemCell.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import DropDown
 
 class ItemCell: UITableViewCell {
 
     static let identfier: String = "ItemCell"
-    
+    var dropDown:DropDown?
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var itemName: UILabel!
     @IBOutlet weak var point: UILabel!
     @IBOutlet weak var cost: UILabel!
+    @IBOutlet weak var button: UIButton!
     
+    @IBOutlet weak var quantity: UILabel!
+    var quanNum:Int = 1
+    
+    @IBAction func minQunatity(_ sender: Any) {
+        if quanNum > 1 {
+            quanNum = quanNum - 1
+            quantity.text = "\(quanNum)"
+        }else{
+            quantity.text = "\(quanNum)"
+        }
+    }
+    @IBAction func addQuantity(_ sender: Any) {
+        quanNum = quanNum + 1
+        quantity.text = "\(quanNum)"
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        dropDown = DropDown()
+        dropDown?.anchorView = button
+        dropDown?.dataSource = ["50 X 90 미네랄", "60 X 100 미네랄"]
+        button.addTarget(self, action: #selector(dropDownButton), for: .touchUpInside)
+        dropDown?.selectionAction = { [unowned self] (index: Int, item: String) in self.button.setTitle(item, for: .normal)
+        }
         // Initialization code
+    }
+    
+    @objc func dropDownButton(){
+        
+        dropDown?.show()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
