c1f0d0651ea6719b0962350d52cda6ff5793c360
seu11ee <yslee2504@naver.com>
2020-06-12 19:08:42 +0900

    이미지성공

--- 6thWeek_Seminar/6thWeek_Seminar/Sources/HomeItemData.swift
+++ 6thWeek_Seminar/6thWeek_Seminar/Sources/HomeItemData.swift
@@ -6,33 +6,50 @@
 //  Copyright © 2020 이예슬. All rights reserved.
 //
 
-import Foundation
-
 struct HomeItemData: Codable {
     var status: Int
     var success: Bool
     var message: String
-    var data: [HomeItemInfo]
+    var data: HomeItemResult?
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        status = (try? values.decode(Int.self, forKey: .status)) ?? -1
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+        data = (try? values.decode(HomeItemResult?.self, forKey: .data)) ?? nil
+    }
 }
 
-// MARK: - Datum
+// MARK: - DataClass
+struct HomeItemResult: Codable {
+    var result: [HomeItemInfo]
+}
+
+// MARK: - Result
 struct HomeItemInfo: Codable {
+    var idx: Int
     var name, content: String
     var like: Int
     var image: String
-    var subProduct: [SubProduct]
+    var categoryIdx: Int
+    var option: [HomeItemOption]
+
+    enum CodingKeys: String, CodingKey {
+        case idx = "Idx"
+        case name, content, like, image, categoryIdx, option
+    }
 }
 
-// MARK: - SubProduct
-struct SubProduct: Codable {
+// MARK: - Option
+struct HomeItemOption: Codable {
     var dName: String
     var dPrice: Int
-    var dSale, dImage: String
+    var dSale: Double
 
     enum CodingKeys: String, CodingKey {
         case dName = "d_name"
         case dPrice = "d_price"
         case dSale = "d_sale"
-        case dImage = "d_image"
     }
 }
